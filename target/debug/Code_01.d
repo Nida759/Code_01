4629b78ce15bf0e164d532e6cf3db795e915a754
Your Name <Your Email>
2019-12-05 07:55:29 +0000

    added commit

---
+++ target/debug/Code_01.d
@@ -0,0 +1,1 @@
+/home/nida/Code_01/Code_01/target/debug/Code_01: /home/nida/Code_01/Code_01/src/main.rs
---
+++ target/debug/deps/Code_01-d59fd69031dc45f5.d
@@ -0,0 +1,5 @@
+/home/nida/Code_01/Code_01/target/debug/deps/Code_01-d59fd69031dc45f5: src/main.rs
+
+/home/nida/Code_01/Code_01/target/debug/deps/Code_01-d59fd69031dc45f5.d: src/main.rs
+
+src/main.rs:
